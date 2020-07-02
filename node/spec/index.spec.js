3af33f2b8032f249aab20a008421a73c9b6e3e33
Simon Oulevay <simon.oulevay@gmail.com>
2020-07-02 22:33:21 +0200

    Add & test node function

--- node/spec/index.spec.js
+++ node/spec/index.spec.js
@@ -1,8 +1,14 @@
-const { add } = require('../');
+const { add, subtract } = require('../');
 const { expect } = require('chai');
 
 describe(add.name, function() {
   it('should add two numbers', function() {
     expect(add(2, 3)).to.equal(5);
+  });
+});
+
+describe(subtract.name, function() {
+  it('should subtract two numbers', function() {
+    expect(subtract(2, 3)).to.equal(-1);
   });
 });
