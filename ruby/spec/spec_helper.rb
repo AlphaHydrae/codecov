86f3ef17cef21b64f8889493d096847380e3f454
Simon Oulevay <simon.oulevay@gmail.com>
2020-07-02 23:40:53 +0200

    Monkey-patch ruby codecov formatter to fix path

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -2,6 +2,13 @@ require 'simplecov'
 SimpleCov.start
 
 require 'codecov'
+
+class SimpleCov::Formatter::Codecov
+  def shortened_filename(file)
+    "ruby/#{super.shortened_filename(file)}"
+  end
+end
+
 SimpleCov.formatter = if ENV['CI'] == 'true'
   SimpleCov::Formatter::Codecov
 else
