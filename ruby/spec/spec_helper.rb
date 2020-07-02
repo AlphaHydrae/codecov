4365949c62216c01f75b91a8b94acb3b008babae
Simon Oulevay <simon.oulevay@gmail.com>
2020-07-02 23:43:33 +0200

    Subclass codecov formatter in ruby tests

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -3,14 +3,14 @@ SimpleCov.start
 
 require 'codecov'
 
-class SimpleCov::Formatter::Codecov
+class CustomCodecovFormatter < SimpleCov::Formatter::Codecov
   def shortened_filename(file)
     "ruby/#{super(file)}"
   end
 end
 
 SimpleCov.formatter = if ENV['CI'] == 'true'
-  SimpleCov::Formatter::Codecov
+  CustomCodecovFormatter
 else
   SimpleCov::Formatter::HTMLFormatter
 end
