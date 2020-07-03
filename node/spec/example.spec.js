7e714651982e8f53d2979f855ba67f7812f1e1e7
Simon Oulevay <simon.oulevay@gmail.com>
2020-07-03 15:57:36 +0200

    Rename node index to example

--- node/spec/example.spec.js
+++ node/spec/example.spec.js
@@ -1,13 +1,13 @@
 const { add, subtract } = require('../');
 const { expect } = require('chai');
 
-describe(add.name, function() {
+describe('add', function() {
   it('should add two numbers', function() {
     expect(add(2, 3)).to.equal(5);
   });
 });
 
-describe(subtract.name, function() {
+describe('subtract', function() {
   it('should subtract two numbers', function() {
     expect(subtract(2, 3)).to.equal(-1);
   });
